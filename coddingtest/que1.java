1a3f62530fae66ae52c5ced9ac5840471ab2c843
PRITAM TRIBHUWAN <pritamtribhuwan@gmail.com>
2020-02-11 12:06:47 +0530

    java

---
+++ coddingtest/que1.java
@@ -0,0 +1,29 @@
+package coddingtest;
+
+public class que1
+{
+    public static void main(String args[])
+    {
+        int i, j,k,l;
+        
+        	
+        for(i=0; i<5; i++)
+        {
+        	for(k=0;k<3;k++)
+        	{
+        		  
+                  	
+                      System.out.print(" ");
+                  }
+        	
+        	
+            for(j=0; j<=i; j++)
+            {
+            	
+                System.out.print("* ");
+            }
+        	
+            System.out.println();
+        	}
+    }
+}
---
+++ coddingtest/que3.java
@@ -0,0 +1,24 @@
+package coddingtest;
+
+import java.util.Scanner;
+public class que3
+{
+    public static void main(String[] args) 
+    {
+        int n, mul;
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter any integer:");
+        n = s.nextInt();
+        que3 obj = new que3();
+        mul = obj.fact(n);
+        System.out.println("Factorial of "+n+" :"+mul);
+    }
+    int fact(int x)
+    {
+        if(x > 1)
+        {
+            return(x * fact(x - 1));
+        }
+        return 1;
+    }
+}
