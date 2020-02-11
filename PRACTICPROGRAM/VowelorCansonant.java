5160c3944723e050cf5456ca248f8edb99f1b7bc
PRITAM TRIBHUWAN <pritamtribhuwan@gmail.com>
2020-02-11 12:19:09 +0530

    java

---
+++ PRACTICPROGRAM/VowelorCansonant.java
@@ -0,0 +1,19 @@
+package PRACTICPROGRAM;
+
+import java.util.Scanner;
+
+public class VowelorCansonant {
+	
+		   public static void main(String args[]){
+		      System.out.println("Enter a character :");
+		      Scanner sc = new Scanner(System.in);
+		      char ch = sc.next().charAt(0);
+		      if(ch == 'a'|| ch == 'e'|| ch == 'i' ||ch == 'o' ||ch == 'u'||ch == ' '){
+		         System.out.println("Given character is an vowel");
+		      }else{
+		         System.out.println("Given character is a consonant");
+		      }
+		   }
+
+}
+
